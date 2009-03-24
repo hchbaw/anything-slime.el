66ccc92a9744527f6cd913aed83ed3603d3fc9a5
Takeshi Banse <takebi@laafc.net>
2009-03-24 22:58:51 +0900

    This peace of code starts working, Hello world.
    Signed-off-by: Takeshi Banse <takebi@laafc.net>

--- slime-anything.el
+++ slime-anything.el
@@ -1,38 +1,64 @@
+;;
+;; TODO
+;; - Narrow slime-describe-symbol's pop-up window when invoked through
+;;   anything-persistent-action.
+;;
+
 (require 'slime)
 (require 'slime-fuzzy)
+(require 'slime-c-p-c)
 (require 'anything)
 (require 'anything-complete)
 
+(add-to-list 'anything-type-attributes
+             '(slime-anything
+               (action . (("Insert" . ac-insert)
+                          ("Describe symbol" . slime-describe-symbol)))
+               (persistent-action . (lambda (sym)
+                                      (with-anything-window
+                                        (slime-describe-symbol sym))))
+               (volatile)))
 
-(defvar anything-slime-simple-complete-candidates nil)
-(defvar anything-slime-simple-complete-source
+(defvar anything-c-source-slime-simple-complete
   '((name . "slime simple complete")
-    (init . (lambda ()
-              (setq anything-slime-simple-complete-candidates
-                    (car
-                     (slime-simple-completions anything-complete-target)))))
-    (candidates . anything-slime-simple-complete-candidates)
-    (action . ac-insert)
-    (volatile)))
-
-(defvar anything-slime-fuzzy-complete-candidates nil)
-(defvar anything-slime-fuzzy-complete-source
+    (candidates
+     . (lambda ()
+         (car (slime-simple-completions anything-complete-target))))
+    (type . slime-anything)))
+(defvar anything-c-source-slime-compound-complete
+  '((name . "slime compound complete")
+    (candidates
+     . (lambda ()
+         (with-current-buffer anything-current-buffer
+           (let* ((end (move-marker (make-marker) (slime-symbol-end-pos)))
+                  (beg (move-marker (make-marker) (slime-symbol-start-pos))))
+             (car (slime-contextual-completions beg end))))))
+    (type . slime-anything)))
+(defvar anything-c-source-slime-fuzzy-complete
   '((name . "slime fuzzy complete")
-    (init . (lambda ()
-              (setq anything-slime-fuzzy-complete-candidates
-                    (mapcar
-                     'car
-                     (car
-                      (slime-fuzzy-completions anything-complete-target))))))
-    (candidates . anything-slime-fuzzy-complete-candidates)
-    (type . ac-insert)
-    (volatile)))
+    (candidates
+     . (lambda ()
+         (mapcar 'car
+                 (car (slime-fuzzy-completions anything-complete-target)))))
+    (type . slime-anything)))
+
+(defvar anything-slime-complete-sources
+  '(anything-c-source-slime-simple-complete
+    anything-c-source-slime-compound-complete
+    anything-c-source-slime-fuzzy-complete))
 
 (defun anything-slime-complete ()
+  "Select a symbol from slime's completion systems."
   (interactive)
   (anything-complete
-   '(anything-slime-simple-complete-source
-     anything-slime-fuzzy-complete-source)
-   (anything-aif (symbol-at-point) (symbol-name it) "")))
+   (if (eq major-mode 'clojure-mode)
+     (remove 'anything-c-source-slime-fuzzy-complete
+             anything-slime-complete-sources)
+     anything-slime-complete-sources)
+   (let* ((end (move-marker (make-marker) (slime-symbol-end-pos)))
+          (beg (move-marker (make-marker) (slime-symbol-start-pos))))
+     (buffer-substring-no-properties beg end))))
+
+;; (define-key slime-mode-map "\C-o" 'anything-slime-complete)
 
 (provide 'slime-anything)
