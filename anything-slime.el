54851fd21ac06c5a5cf3c488ed65e365d18e2b7c
Takeshi Banse <takebi@laafc.net>
2009-07-02 14:48:13 +0900

    Cleanups.
    Signed-off-by: Takeshi Banse <takebi@laafc.net>

--- anything-slime.el
+++ anything-slime.el
@@ -1,14 +1,12 @@
-;;; anything-slime.el --- anything-sources and some utilities for slime.
+;;; anything-slime.el --- anything-sources and some utilities for SLIME.
 
 ;; Copyright (C) 2009 Takeshi Banse <takebi@laafc.net>
-
 ;; Author: Takeshi Banse <takebi@laafc.net>
-;; Keywords: anything, slime
-;; URL: http://www.emacswiki.org/cgi-bin/wiki/anything-slime.el
+;; Keywords: convenience, anything, slime
 
-;; This file is free software; you can redistribute it and/or modify
+;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
+;; the Free Software Foundation; either version 3, or (at your option)
 ;; any later version.
 
 ;; This file is distributed in the hope that it will be useful,
@@ -17,30 +15,39 @@
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to the
-;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 ;; Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
+;;
+;; Some Anything and SLIME Configurations for using SLIME within the
+;; Anything interface. (The `ascsa-' prefix comes here.)
 
 ;;; Installation:
 ;;
-;; Put anything-slime.el, anything.el and anything-complete.el to your
-;; load-path. Also set up slime properly. Finally call `slime-setup' and
-;; include 'anything-slime as argument:
+;; Put the anything-slime.el, anything.el and anything-complete.el to your
+;; load-path.
+;; Set up the SLIME properly.
+;; Call `slime-setup' and include 'anything-slime as the arguments:
 ;;
 ;;   (slime-setup '([others contribs ...] anything-slime))
 ;;
+;; or simply require anything-slime.
 
 ;;; Commands:
 ;;
 ;; Below are complete command list:
 ;;
 ;;  `anything-slime-complete'
-;;    Select a symbol from slime's completion systems.
+;;    Select a symbol from the SLIME's completion systems.
 ;;  `anything-slime-list-connections'
 ;;    Yet another `slime-list-connections' with `anything'.
+;;  `anything-slime-apropos'
+;;    Yet another `slime-apropos' with `anything'.
 ;;
+
+;;; Code:
 
 (require 'anything)
 (require 'anything-complete)
@@ -49,15 +56,13 @@
 (require 'slime-fuzzy)
 (require 'slime-repl)
 
-(defmacro with-anything-slime-symbol-position (beg end &rest body)
-  `(let* ((,end (move-marker (make-marker) (slime-symbol-end-pos)))
-          (,beg (move-marker (make-marker) (slime-symbol-start-pos))))
-     (unwind-protect (progn ,@body)
-       (set-marker ,end nil)
-       (set-marker ,beg nil))))
-(put 'with-anything-slime-symbol-position 'lisp-indent-function 2)
-(defun anything-slime-symbol-position-funcall (f)
-  (with-anything-slime-symbol-position beg end (funcall f beg end)))
+(defun ascsa-symbol-position-funcall (f)
+  (let* ((end (move-marker (make-marker) (point)))
+         (beg (move-marker (make-marker) (slime-symbol-start-pos))))
+    (unwind-protect
+        (progn (funcall f beg end))
+      (set-marker end nil)
+      (set-marker beg nil))))
 
 (define-anything-type-attribute 'anything-slime-complete
   '((action
@@ -66,30 +71,30 @@
         ("Edit definition" . slime-edit-definition)))
     (persistent-action . slime-describe-symbol)
     (volatile))
-  "Slime complete.")
+  "SLIME complete.")
 
-;; I do not want to make anything-c-source-* symbols because they are
-;; private in `anything-slime-complete'.
+;; These sources are private for the use of the `anything-slime-complete'
+;; command, so I should not make `anything-c-source-*' symbols.
 (defvar anything-slime-simple-complete-source
-  '((name . "slime simple complete")
+  '((name . "SLIME simple complete")
     (candidates
      . (lambda ()
          (car (slime-simple-completions anything-complete-target))))
     (type . anything-slime-complete)))
+(defvar anything-slime-fuzzy-complete-source
+  '((name . "SLIME fuzzy complete")
+    (candidates
+     . (lambda ()
+         (mapcar #'car
+                 (car (slime-fuzzy-completions anything-complete-target)))))
+    (type . anything-slime-complete)))
 (defvar anything-slime-compound-complete-source
-  '((name . "slime compound complete")
+  '((name . "SLIME compound complete")
     (candidates
      . (lambda ()
          (with-current-buffer anything-current-buffer
-           (car (anything-slime-symbol-position-funcall
+           (car (ascsa-symbol-position-funcall
                  #'slime-contextual-completions)))))
-    (type . anything-slime-complete)))
-(defvar anything-slime-fuzzy-complete-source
-  '((name . "slime fuzzy complete")
-    (candidates
-     . (lambda ()
-         (mapcar 'car
-                 (car (slime-fuzzy-completions anything-complete-target)))))
     (type . anything-slime-complete)))
 
 (defvar anything-slime-complete-sources
@@ -98,14 +103,14 @@
     anything-slime-compound-complete-source))
 
 (defun anything-slime-complete ()
-  "Select a symbol from slime's completion systems."
+  "Select a symbol from the SLIME's completion systems."
   (interactive)
   (anything-complete anything-slime-complete-sources
-                     (anything-slime-symbol-position-funcall
+                     (ascsa-symbol-position-funcall
                       #'buffer-substring-no-properties)))
 
 (defvar anything-c-source-slime-connection
-  '((name . "slime connections")
+  '((name . "SLIME connections")
     (candidates
      . (lambda ()
          (let* ((default slime-default-connection)
@@ -130,12 +135,10 @@
   (interactive)
   (anything 'anything-c-source-slime-connection))
 
-(defadvice anything-slime-update-connection-list
-  (around slime-update-connection-list)
+(defadvice anything-slime-update-connection-list (around ignore activate)
   "Don't call slime-update-connection-list if anythinging. (This is iffy.)"
   (when (not anything-source-name)
     ad-do-it))
-(ad-activate 'anything-slime-update-connection-list)
 
 (define-anything-type-attribute 'anything-slime-apropos
   '((action
@@ -143,43 +146,62 @@
         ("Edit definition" . slime-edit-definition)))
     (persistent-action . slime-describe-symbol)
     (requires-pattern . 2))
-  "Slime apropos.")
+    ;;(volatile)
+  "SLIME apropos.")
 
-(defun anything-slime-apropos-source (name &rest slime-expressions)
+(defun ascsa-apropos-source (name slime-expressions)
   `((name . ,name)
     (candidates
      . (lambda ()
          (with-current-buffer anything-current-buffer
-           (loop for plist in (slime-eval ,@slime-expressions)
+           (loop for plist in (slime-eval ,slime-expressions)
                  collect (plist-get plist :designator)))))
     (type . anything-slime-apropos)))
 (defvar anything-c-source-slime-apropos-symbol-current-package
-  (anything-slime-apropos-source "slime apropos (current package)"
-                                 '`(swank:apropos-list-for-emacs
-                                    ,anything-pattern
-                                    nil
-                                    nil
-                                    ,(or slime-buffer-package
-                                         (slime-current-package)))))
+  (ascsa-apropos-source "SLIME apropos (current package)"
+                        (quote
+                         `(swank:apropos-list-for-emacs
+                           ,anything-pattern
+                           nil
+                           nil
+                           ,(or slime-buffer-package
+                                (slime-current-package))))))
 (defvar anything-c-source-slime-apropos-symbol-external-package
-  (anything-slime-apropos-source "slime apropos (external package)"
-                                 '`(swank:apropos-list-for-emacs
-                                    ,anything-pattern
-                                    t
-                                    nil
-                                    ,(or slime-buffer-package
-                                         (slime-current-package)))))
-(defvar anything-c-source-slime-apropos-symbol-all
-  (anything-slime-apropos-source "slime apropos (all)"
-                                 '`(swank:apropos-list-for-emacs
-                                    ,anything-pattern
-                                    nil
-                                    nil
-                                    nil)))
+  (ascsa-apropos-source "SLIME apropos (external package)"
+                        (quote
+                         `(swank:apropos-list-for-emacs
+                           ,anything-pattern
+                           t
+                           nil
+                           ,(or slime-buffer-package
+                                (slime-current-package))))))
+(defvar anything-c-source-slime-apropos-symbol-all-external-package
+  (ascsa-apropos-source "SLIME apropos (all external package)"
+                        (quote
+                         `(swank:apropos-list-for-emacs
+                           ,anything-pattern
+                           t
+                           nil
+                           nil))))
+(defvar anything-c-source-slime-apropos-symbol-all-package
+  (ascsa-apropos-source "SLIME apropos (all package)"
+                        (quote
+                         `(swank:apropos-list-for-emacs
+                           ,anything-pattern
+                           nil
+                           nil
+                           nil))))
 (defvar anything-slime-apropos-sources
   '(anything-c-source-slime-apropos-symbol-current-package
     anything-c-source-slime-apropos-symbol-external-package
-    anything-c-source-slime-apropos-symbol-all))
+    anything-c-source-slime-apropos-symbol-all-external-package
+    anything-c-source-slime-apropos-symbol-all-package))
+
+(defun anything-slime-apropos ()
+  "Yet another `slime-apropos' with `anything'."
+  (interactive)
+  (anything anything-slime-apropos-sources
+            nil nil nil nil "*anything SLIME apropos*"))
 
 (defun anything-slime-init ()
   (run-hooks 'anything-slime-init-hook))
